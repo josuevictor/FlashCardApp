349cb4e766f63fc076c0307d5292eb1b244ec92e
josuevictor-notebook <victorarroxellas01@gmail.com>
2022-01-20 16:35:14 -0300

    Funçao botao disciplinas

--- scripts.js
+++ scripts.js
@@ -33,7 +33,7 @@ const newCard={
     insert(){
         document.querySelector('#discipline')
         .innerHTML = `<div class="card">
-                        oi
+                        te amo <3
                       </div>`
     }
 }
